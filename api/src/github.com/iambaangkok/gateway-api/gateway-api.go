d29c5f44954bcaebf2c4451f3ff0dec91dac8717
Baangkok Vanijyananda <iambaangkok@gmail.com>
2024-02-05 21:14:21 +0700

    add: mysql, kafka stack

---
+++ api/src/github.com/iambaangkok/gateway-api/gateway-api.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Request Hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+
+func main() {
+	handleRequests()
+}
